b88f020692b99465f680ea4d586c8edbb39a3d6b
André <andrebq@gmail.com>
2020-07-13 17:57:21 +0200

    keep the old lts until I fix the plugin issue

--- jenkins-master-lite/boot-sequence.sh
+++ jenkins-master-lite/boot-sequence.sh
@@ -13,10 +13,20 @@ function install-selected-plugins {
     su -c 'bash /usr/local/bin/install-plugins.sh < /var/jenkins/boot/plugins.txt'
 }
 
+function install-user-plugins {
+    declare -r userPluginsPath="/var/jenkins/user-boot/plugins.txt"
+    if [[ ! -f "${userPluginsPath}" ]]; then
+        echo "File ${userPluginsPath} does not exists. No custom plugins"
+        return 0
+    fi
+    su -c "bash /usr/local/bin/install-plugins.sh < ${userPluginsPath}"
+}
+
 function start-jenkins {
     su -c 'bash /usr/local/bin/jenkins.sh'
 }
 
 setup-jenkins-home-permissions
 install-selected-plugins
+install-user-plugins
 start-jenkins
